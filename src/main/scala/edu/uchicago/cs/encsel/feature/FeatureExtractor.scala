264a24154deddba224fde08304d1837597b63c32
Harper <harperjiang@msn.com>
2017-01-30 05:22:24 -0600

    Refactoring

--- src/main/scala/edu/uchicago/cs/encsel/feature/FeatureExtractor.scala
+++ src/main/scala/edu/uchicago/cs/encsel/feature/FeatureExtractor.scala
@@ -2,7 +2,8 @@ package edu.uchicago.cs.encsel.feature
 
 import java.io.File
 import java.net.URI
+import edu.uchicago.cs.encsel.model.Column
 
 trait FeatureExtractor {
-  def extract(input: URI): Iterable[Feature]
+  def extract(input: Column): Iterable[Feature]
 }
