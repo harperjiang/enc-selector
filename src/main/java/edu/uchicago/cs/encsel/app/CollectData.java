da85e9d01b56384ef43746db4fa7afe8eca68cbc
Harper Jiang <harperjiang@msn.com>
2017-02-01 09:09:10 -0600

    fix scan dir bug

--- src/main/java/edu/uchicago/cs/encsel/app/CollectData.java
+++ src/main/java/edu/uchicago/cs/encsel/app/CollectData.java
@@ -6,9 +6,10 @@ import edu.uchicago.cs.encsel.datacol.DataCollector;
 
 public class CollectData {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		File f = new File(args[0]);
 		new DataCollector().collect(f.toURI());
+
 	}
 
 }
