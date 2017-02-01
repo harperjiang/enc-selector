cd621e5c8a253be9465ee7c8b8b0885f1fa6b8f2
Harper Jiang <harperjiang@msn.com>
2017-02-01 08:50:25 -0600

    package and run

---
+++ src/main/java/edu/uchicago/cs/encsel/app/CollectData.java
@@ -0,0 +1,14 @@
+package edu.uchicago.cs.encsel.app;
+
+import java.io.File;
+
+import edu.uchicago.cs.encsel.datacol.DataCollector;
+
+public class CollectData {
+
+	public static void main(String[] args) {
+		File f = new File(args[0]);
+		new DataCollector().collect(f.toURI());
+	}
+
+}
