7eeebd910d49205fe8421dd127f496f5bbaada91
Harper Jiang <harperjiang@msn.com>
2017-02-01 09:42:52 -0600

    ignore parsing errors

--- src/main/scala/edu/uchicago/cs/encsel/colread/Parser.scala
+++ src/main/scala/edu/uchicago/cs/encsel/colread/Parser.scala
@@ -2,19 +2,33 @@ package edu.uchicago.cs.encsel.colread
 
 import scala.io.Source
 import java.net.URI
+import org.slf4j.LoggerFactory
 
 trait Parser {
 
   var schema: Schema = null
 
+  var logger = LoggerFactory.getLogger(getClass())
+
   def parse(inputFile: URI, schema: Schema): Iterable[Array[String]] = {
     this.schema = schema
-    return Source.fromFile(inputFile).getLines().filter(!_.trim().isEmpty()).map { parseLine(_) }.toIterable
+    return Source.fromFile(inputFile).getLines().filter(!_.trim().isEmpty()).map { parseLineIgnoreError(_) }.toIterable
   }
 
   def parse(inputString: String, schema: Schema): Iterable[Array[String]] = {
     this.schema = schema
-    return inputString.split("[\r\n]+").map { parseLine(_) }.toIterable
+    return inputString.split("[\r\n]+").map { parseLineIgnoreError(_) }.toIterable
+  }
+
+  def parseLineIgnoreError(line: String): Array[String] = {
+    try {
+      return parseLine(line)
+    } catch {
+      case e: Exception => {
+        logger.warn("Exception while parsing line:" + line, e)
+        return Array[String]()
+      }
+    }
   }
 
   def parseLine(line: String): Array[String]
