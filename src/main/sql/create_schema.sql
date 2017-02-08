54b06ab73e81de013b2921dca7705bbcf767d5aa
Harper Jiang <harperjiang@msn.com>
2017-02-07 22:11:54 -0600

    jpa test case

--- src/main/sql/create_schema.sql
+++ src/main/sql/create_schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
 --
--- Host: localhost    Database: encsel
+-- Host: localhost    Database: encsel_test
 -- ------------------------------------------------------
--- Server version	5.5.54-0ubuntu0.14.04.1
+-- Server version	5.7.17-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +30,7 @@ CREATE TABLE `col_data` (
   `data_type` varchar(20) DEFAULT NULL,
   `origin_uri` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `col_data` (
 
 LOCK TABLES `col_data` WRITE;
 /*!40000 ALTER TABLE `col_data` DISABLE KEYS */;
+INSERT INTO `col_data` VALUES (-1,'a',5,'file:/home/harper/workspace/enc-selector/aab','STRING','file:/home/harper/workspace/enc-selector/ccd'),(1,'a',5,'file:/home/harper/workspace/enc-selector/aab','STRING','file:/home/harper/workspace/enc-selector/ccd'),(2,'a',5,'file:/home/harper/workspace/enc-selector/aab','STRING','file:/home/harper/workspace/enc-selector/ccd'),(3,'a',5,'file:/home/harper/workspace/enc-selector/aab','STRING','file:/home/harper/workspace/enc-selector/ccd');
 /*!40000 ALTER TABLE `col_data` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,13 +51,12 @@ DROP TABLE IF EXISTS `feature`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feature` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `col_id` int(11) DEFAULT NULL,
-  `name` varchar(150) DEFAULT NULL,
+  `col_id` int(11) NOT NULL,
+  `type` varchar(150) NOT NULL,
+  `name` varchar(150) NOT NULL,
   `value` decimal(20,10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_feature_col_id` (`col_id`),
-  CONSTRAINT `fk_feature_col_id` FOREIGN KEY (`col_id`) REFERENCES `col_data` (`id`)
+  PRIMARY KEY (`col_id`,`type`,`name`),
+  CONSTRAINT `fk_feature_col_id` FOREIGN KEY (`col_id`) REFERENCES `col_data` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,6 +66,7 @@ CREATE TABLE `feature` (
 
 LOCK TABLES `feature` WRITE;
 /*!40000 ALTER TABLE `feature` DISABLE KEYS */;
+INSERT INTO `feature` VALUES (-1,'W','A',3.5000000000),(1,'W','A',3.5000000000),(2,'W','A',3.5000000000),(3,'W','A',3.5000000000);
 /*!40000 ALTER TABLE `feature` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -78,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-07 20:02:15
+-- Dump completed on 2017-02-07 21:37:38
