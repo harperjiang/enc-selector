46bbaf06242201903cf1d9986d54dc4ad2133c43
Harper Jiang <harperjiang@msn.com>
2017-02-14 12:43:54 -0600

    Dictionary Available

--- src/test/scala/edu/uchicago/cs/encsel/wordvec/WordSplitTest.scala
+++ src/test/scala/edu/uchicago/cs/encsel/wordvec/WordSplitTest.scala
@@ -1,13 +1,33 @@
 package edu.uchicago.cs.encsel.wordvec
 
 import org.junit.Test
+import org.junit.Assert._
 
 class WordSplitTest {
 
   @Test
   def testSplit(): Unit = {
     var split = new WordSplit()
-    var res = split.split("RPTDT")
+    var res = split.split("RPTYR")
+    assertEquals(2, res._1.length)
+    assertEquals("report", res._1(0))
+    assertEquals("year", res._1(1))
+
+  }
+
+  @Test
+  def testSplitLong: Unit = {
+    var split = new WordSplit()
+    var res = split.split("inspectioncode")
+    assertEquals(2, res._1.length)
+    assertEquals("inspection", res._1(0))
+    assertEquals("code", res._1(1))
+
+    res = split.split("actualcmd")
     println(res)
+    assertEquals(2, res._1.length)
+    assertEquals("actual", res._1(0))
+    assertEquals("command", res._1(1))
+
   }
 }
