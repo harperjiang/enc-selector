0a118393d500258568714de3dce211d256e7e067
Harper Jiang <harperjiang@msn.com>
2017-02-17 17:21:20 -0600

    ND4j

--- src/main/scala/edu/uchicago/cs/encsel/network/Model.scala
+++ src/main/scala/edu/uchicago/cs/encsel/network/Model.scala
@@ -1,7 +1,5 @@
 package edu.uchicago.cs.encsel.network
 
-import java.util
-
 import scala.collection.mutable.ArrayBuffer
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
@@ -17,8 +15,8 @@ abstract class Model(is: Model*) {
   protected var readyInput = new HashSet[Model]
   protected var readyOutput = new HashSet[Model]
 
-  protected var value: INDArray = null
-  protected var grad: INDArray = null
+  private[network] var value: INDArray = null
+  private[network] var grad: INDArray = null
 
   inputs ++= is
   inputs.foreach(_.addOutput(this))
@@ -56,54 +54,55 @@ abstract class Model(is: Model*) {
 }
 
 class Input extends Model() {
-  def setValue(value:INDArray) = this.value = value
+  def setValue(value: INDArray) = this.value = value
   def compute = this.value
   def updateGrad = Unit
 }
 
-class Output(input:Model) extends Model(input) {
+class Output(input: Model) extends Model(input) {
   def getValue() = this.value
-  def setGrad(grad:INDArray):Unit = this.grad = grad
+  def setGrad(grad: INDArray): Unit = this.grad = grad
   def compute = null
-  def updateGrad = {input.grad = this.grad}
+  def updateGrad = { input.grad = this.grad }
 }
 
-class Add(x:Model, y:Model) extends Model(x,y) {
+class Add(x: Model, y: Model) extends Model(x, y) {
   protected val left = x
   protected val right = y
 
-  def compute:INDArray = {
+  def compute: INDArray = {
     // Always assume y is a smaller
-    if(right.value.shape.sameElements(right.value.shape)) {
+    if (right.value.shape.sameElements(right.value.shape)) {
       left.value.addi(right.value)
     } else {
       left.value.addiColumnVector(right.value)
     }
   }
 
-  def updateGrad:Unit = {
+  def updateGrad: Unit = {
     // TODO Sum along dimension
     left.grad = this.grad
     right.grad = this.grad
   }
 }
 
-class DotMul(x:Model,y:Model) extends Model(x,y) {
+class DotMul(x: Model, y: Model) extends Model(x, y) {
 
   protected val left = x
   protected val right = y
 
-  def compute:INDArray = left.value.muli(right.value)
+  def compute: INDArray = left.value.muli(right.value)
 
-  def updateGrad:Unit= {
-      left.grad = this.grad.muli(right.value)
-      right.grad = left.value.muli(this.grad)
+  def updateGrad: Unit = {
+    left.grad = this.grad.muli(right.value)
+    right.grad = left.value.muli(this.grad)
   }
 }
 
-class Mul(x:Model,y:Model) extends Model(x,y) {
+class Mul(x: Model, y: Model) extends Model(x, y) {
   protected val left = x
   protected val right = y
 
-  def compute:INDArray = left.value.mul(right.value)
+  def compute: INDArray = left.value.mul(right.value)
+  def updateGrad: Unit = {}
 }
--- src/main/scala/edu/uchicago/cs/encsel/network/ND4JSample.scala
+++ src/main/scala/edu/uchicago/cs/encsel/network/ND4JSample.scala
@@ -3,10 +3,12 @@ package edu.uchicago.cs.encsel.network
 import org.nd4j.linalg.factory.Nd4j
 
 /**
-  * Created by harper on 2/17/17.
-  */
+ * Created by harper on 2/17/17.
+ */
 object ND4JSample extends App {
 
-  var a = Nd4j.create(Array(Array(1.0,2.0),Array(3.0,4.0)))
-  println(a)
+  var a = Nd4j.createUninitialized(Array(3,2,4,4))
+  var b = Nd4j.createUninitialized(Array(2,4,4))
+  
+  a.add(b.broadcast(3))
 }
