44be1009df636c142aa4e6148c74f351ef119123
Harper Jiang <harperjiang@msn.com>
2017-03-08 09:45:25 -0600

    add carbon app

--- src/main/scala/edu/uchicago/cs/encsel/app/carbon/CompareWithCarbon.scala
+++ src/main/scala/edu/uchicago/cs/encsel/app/carbon/CompareWithCarbon.scala
@@ -22,51 +22,44 @@
 
 package edu.uchicago.cs.encsel.app.carbon
 
-import java.io.{File, FileInputStream}
+import java.io.{ File, FileInputStream }
 import java.net.URI
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{ Files, Path, Paths }
 
 import edu.uchicago.cs.encsel.dataset.parser.csv.CommonsCSVParser
 import edu.uchicago.cs.encsel.dataset.schema.Schema
 import edu.uchicago.cs.encsel.model.DataType
 
 import scala.collection.JavaConversions._
+import edu.uchicago.cs.encsel.dataset.persist.Persistence
+import edu.uchicago.cs.encsel.dataset.persist.jpa.JPAPersistence
 
 object CompareWithCarbon extends App {
 
-  val csvfile = "/home/harper/enc_workspace/carbon/dict_is_good.csv"
-  val schema = new Schema(Array((DataType.STRING, "id"), (DataType.STRING, "loc"), (DataType.STRING, "type"), (DataType.DOUBLE, "psize"), (DataType.DOUBLE, "dsize")), true)
+  val idfile = "/home/hajiang/carbon_id"
+  val schema = new Schema(Array((DataType.STRING, "id")), false)
+  val carbonRoot = "/home/hajiang/incubator-carbondata/bin/carbonshellstore/default"
 
   compareSize
 
-  def genCarbonSQL: Unit = {
-    val parser = new CommonsCSVParser
-    val records = parser.parse(new FileInputStream(csvfile), schema)
-    val sql1 = """cc.sql("create table if not exists tab_%d (id string) STORED BY 'carbondata'")"""
-    val sql2 = """cc.sql("load data inpath '/home/harper/enc_workspace/carbon/%d.tmp' into table tab_%d")"""
-
-    records.foreach(rec => {
-      val id = rec(0).toInt
-      println(sql1.format(id))
-      println(sql2.format(id, id))
-    })
-  }
-
   def compareSize: Unit = {
     val parser = new CommonsCSVParser
-    val records = parser.parse(new FileInputStream(csvfile), schema)
-    val carbondir = Paths.get(new File("/home/harper/Repositories/incubator-carbondata/bin/carbonshellstore/default").toURI)
+    val records = parser.parse(new FileInputStream(idfile), schema)
+    val carbondir = Paths.get(new File(carbonRoot).toURI)
+    val persist = Persistence.get.asInstanceOf[JPAPersistence]
 
     records.foreach {
       rec =>
         {
           val id = rec(0)
+          val col = persist.find(id.toInt)
           val tabledir = carbondir.resolve("tab_" + id)
           val tablesize = folderSize(tabledir)
-          val plainsize = rec(3)
-          val dictsize = rec(4)
-          val uri = Paths.get(new URI(rec(1)))
-          println("%s,%s,%s,%s,%d".format(id,uri.getFileName.toString(), plainsize, dictsize, tablesize))
+          val plainsize = col.findFeature("EncFileSize", "PLAIN_file_size").value.toInt
+          val dictsize = col.findFeature("EncFileSize", "DICT_file_size").value.toInt
+          val distinct = col.findFeature("Distinct", "Distinct").value.toInt
+
+          println("%s,%d,%d,%d,%d".format(id, plainsize, dictsize, tablesize, distinct))
         }
     }
   }
