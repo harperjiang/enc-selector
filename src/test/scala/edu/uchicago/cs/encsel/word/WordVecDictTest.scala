fd36e2fc36007af50191fdc9c2f406e888b6f9ce
Harper Jiang <harperjiang@msn.com>
2017-03-10 16:19:32 -0600

    word vector

---
+++ src/test/scala/edu/uchicago/cs/encsel/word/WordVecDictTest.scala
@@ -0,0 +1,20 @@
+package edu.uchicago.cs.encsel.word
+
+import org.junit.Test
+
+class WordVecDictTest {
+
+  @Test
+  def testFind: Unit = {
+    val words = new WordVecDict("/home/harper/Downloads/glove.42B.300d.txt")
+
+    println(words.compare("st.", "rd."))
+    println(words.compare("street", "st."))
+    println(words.compare("road", "rd."))
+    println(words.compare("avenue", "ave."))
+    println(words.compare("road", "ave."))
+    println(words.compare("street", "rd."))
+    println(words.compare("street", "box"))
+    println(words.compare("street", "ap"))
+  }
+}
