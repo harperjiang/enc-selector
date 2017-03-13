ee2657789c238585e0524541b78c629424dfbdff
Harper Jiang <harperjiang@msn.com>
2017-03-13 16:35:51 -0500

    word group

---
+++ src/test/scala/edu/uchicago/cs/encsel/colpattern/WordEmbedDictTest.scala
@@ -0,0 +1,39 @@
+package edu.uchicago.cs.encsel.colpattern
+
+import org.junit.Test
+import org.junit.Assert._
+
+class WordEmbedDictTest {
+
+  @Test
+  def testFind: Unit = {
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+
+    assertTrue(!words.find("st.").isEmpty)
+  }
+
+  @Test
+  def testCompare: Unit = {
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+
+    println(words.compare("street","avenue"))
+    assertTrue(0.5 < words.compare("street", "avenue"))
+  }
+
+  @Test
+  def testAddPhrase: Unit = {
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+
+    assertTrue(words.find("good man").isEmpty)
+
+    words.addPhrase("good man",Array("good","man"))
+
+    assertTrue(!words.find("good man").isEmpty)
+
+    val result = words.find("good man").get
+    val good = words.find("good").get
+    val man = words.find("man").get
+
+    assertEquals(result, good.add(man))
+  }
+}
