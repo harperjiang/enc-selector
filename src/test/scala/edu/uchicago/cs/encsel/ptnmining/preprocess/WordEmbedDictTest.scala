ff7bf2e9e4ee3dc766ca4d2aeb61dd1491beb1eb
Hao Jiang <harperjiang@msn.com>
2017-04-04 01:59:14 -0500

    Pattern Mine

--- src/test/scala/edu/uchicago/cs/encsel/ptnmining/preprocess/WordEmbedDictTest.scala
+++ src/test/scala/edu/uchicago/cs/encsel/ptnmining/preprocess/WordEmbedDictTest.scala
@@ -7,29 +7,26 @@ class WordEmbedDictTest {
 
   @Test
   def testOutput:Unit = {
-    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
-    println(words.find("rhode"))
-    println(words.find("island"))
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.840B.300d.txt")
   }
 
   @Test
   def testFind: Unit = {
-    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.840B.300d.txt")
 
     assertTrue(words.find("st.").isDefined)
   }
 
   @Test
   def testCompare: Unit = {
-    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.840B.300d.txt")
 
-    println(words.compare("street","avenue"))
     assertTrue(0.5 < words.compare("street", "avenue"))
   }
 
   @Test
   def testAddPhrase: Unit = {
-    val words = new WordEmbedDict("/home/harper/Downloads/glove.42B.300d.txt")
+    val words = new WordEmbedDict("/home/harper/Downloads/glove.840B.300d.txt")
 
     assertTrue(words.find("good man").isEmpty)
 
