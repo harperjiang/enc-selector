5e2a2eb701a6104c1119034f1c8491aa16a9a20a
Hao Jiang <harperjiang@msn.com>
2017-04-13 22:59:08 -0500

    group log support malformated string

--- src/main/scala/edu/uchicago/cs/encsel/dataset/preprocess/ubiqlog/GroupLog.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/preprocess/ubiqlog/GroupLog.scala
@@ -96,7 +96,7 @@ class JsonFormatter(lines: Iterator[String]) extends Iterator[String] {
 
 object GroupLog extends App {
   //val root = "/local/hajiang/dataset/uci_repo/UbiqLog4UCI"
-  val root = "/home/harper/test"
+  val root = "/Users/harper/ttt"
 
   val supportedEncodings = Array("utf-8", "iso-8859-1")
   val jsonParser = new JsonParser()
