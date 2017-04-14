c923f8f47014860cb6cafa1d204f39644f063610
Harper Jiang <harperjiang@msn.com>
2017-04-14 13:14:07 -0500

    new feature

--- src/main/scala/edu/uchicago/cs/encsel/dataset/feature/Distinct.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/feature/Distinct.scala
@@ -30,7 +30,9 @@ object Distinct extends FeatureExtractor {
   def featureType = "Distinct"
 
   def extract(col: Column): Iterable[Feature] = {
+    val sum = Source.fromFile(new File(col.colFile)).getLines().count(_ => true)
     val size = Source.fromFile(new File(col.colFile)).getLines().toSet.size
-    Array(new Feature(featureType, "Distinct", size))
+    Array(new Feature(featureType, "distinct", size),
+      new Feature(featureType, "distinct_ratio", size.toDouble / sum))
   }
 }
