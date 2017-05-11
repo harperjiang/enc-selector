00e56880bc6079b174d18c6c81f53e043a2c1482
Hao Jiang <harperjiang@msn.com>
2017-05-11 01:11:17 -0500

    Add InvertPair feature to CollectFeature

--- src/main/scala/edu/uchicago/cs/encsel/dataset/feature/Features.scala
+++ src/main/scala/edu/uchicago/cs/encsel/dataset/feature/Features.scala
@@ -45,6 +45,7 @@ object Features {
   install(new Sortness(50))
   install(new Sortness(100))
   install(new Sortness(200))
+  install(AdjInvertPair)
 
   def install(fe: FeatureExtractor) = {
     extractors += fe
