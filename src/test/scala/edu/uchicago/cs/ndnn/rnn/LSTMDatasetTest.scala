4f7b3c593d430c8c90034ae5cbda85fa2bc4a278
Hao Jiang <harperjiang@msn.com>
2017-05-31 01:28:03 -0500

    lstm dataset test

--- src/test/scala/edu/uchicago/cs/ndnn/rnn/LSTMDatasetTest.scala
+++ src/test/scala/edu/uchicago/cs/ndnn/rnn/LSTMDatasetTest.scala
@@ -19,3 +19,28 @@ class LSTMDatasetTest {
     assertEquals(8, batches.map(_.size).sum)
   }
 }
+
+class S2SDatasetTest {
+  @Test
+  def testLoad: Unit = {
+    val file = "src/test/resource/rnn/s2s_sample_ds"
+    val ds = new S2SDataset(file, new WordTokenizer(), "<s>", "</s>")
+
+    assertEquals(6, ds.dataSize)
+    assertEquals(10, ds.encDictSize)
+    assertEquals(13, ds.decDictSize)
+
+    val batches = ds.batches(5).toArray
+
+    assertEquals(6, batches.length)
+
+    batches.foreach(batch => {
+      val enc = batch.feature(0)
+      val dec = batch.feature(1)
+      val eshape = enc.shape()
+      val dshape = dec.shape()
+      assertEquals(1, eshape(0))
+      assertEquals(1, dshape(0))
+    })
+  }
+}
