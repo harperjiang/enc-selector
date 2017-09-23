d5814d23fcc2904a9836abf3f674debf560bd197
Hao Jiang <harperjiang@msn.com>
2017-09-23 13:47:40 -0500

    Unit Test for BitVectorEncoding

--- src/main/scala/edu/uchicago/cs/encsel/encoding/BitVectorEncoding.scala
+++ src/main/scala/edu/uchicago/cs/encsel/encoding/BitVectorEncoding.scala
@@ -26,25 +26,26 @@ import java.io.{File, RandomAccessFile}
 import java.net.URI
 import java.nio.MappedByteBuffer
 import java.nio.channels.FileChannel.MapMode
+import java.nio.charset.StandardCharsets
 
 import com.google.gson.{Gson, JsonObject}
 import edu.uchicago.cs.encsel.dataset.column.Column
 
 import scala.collection.mutable
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 
 class BitVectorEncoding extends Encoding {
 
   override def encode(input: Column, output: URI): Unit = {
     val source = Source.fromFile(input.colFile)
-
+    val source2 = Source.fromFile(input.colFile)
     try {
       val dict = new mutable.HashMap[String, Int]();
       var counter = 0L;
       // First pass, generate dict and count length
-      source.getLines().foreach({
+      source.getLines().foreach(l => {
         counter += 1;
-        dict.getOrElseUpdate(_, dict.size)
+        dict.getOrElseUpdate(l, dict.size)
       })
       // Store dict as json object
       var jsondict = new JsonObject;
@@ -52,7 +53,7 @@ class BitVectorEncoding extends Encoding {
         jsondict.addProperty(f._1, f._2);
       })
       var dictstr = new Gson().toJson(jsondict)
-
+      var dictbytes = dictstr.getBytes(StandardCharsets.UTF_8)
 
       // Compute file size, allocate space
       val bitvecSize = Math.ceil(counter.toDouble / 8).toInt
@@ -60,20 +61,31 @@ class BitVectorEncoding extends Encoding {
       val outputFile = new RandomAccessFile(new File(output), "rw")
 
       // 64 bit for dictionary offset
+      // 64 bit for number of items
       // bitmap
       // dictionary
-      val fileSize = 8 + bitmapSize + dictstr.length
-      outputFile.setLength(fileSize)
+      val bitmapOffset = 8 + 8L
+      // Write dictionary
+      val fileSize = bitmapOffset + bitmapSize + dictbytes.length
+
+      outputFile.seek(0)
+      outputFile.writeLong(bitmapOffset + bitmapSize)
+      outputFile.writeLong(counter)
+
+      // Write dictionary
+      val dictBuffer = outputFile.getChannel.map(MapMode.READ_WRITE, bitmapOffset + bitmapSize, dictbytes.length);
+      dictBuffer.put(dictbytes)
+      dictBuffer.force()
 
       // Second pass, write bit vectors
-      var pos = 0L;
+      var pos = bitmapOffset;
       val size = 1024 * 1024;
       var buffer = outputFile.getChannel.map(MapMode.READ_WRITE, pos, size);
       buffer.load();
 
 
       var offset = 0l
-      source.getLines().foreach(line => {
+      source2.getLines().foreach(line => {
         val idx = dict.getOrElse(line, -1)
 
         val byteOffset = idx * bitvecSize + offset / 8
@@ -83,7 +95,7 @@ class BitVectorEncoding extends Encoding {
           // Write back
           buffer.force()
           // Load new buffer
-          pos = (byteOffset / size) * size
+          pos = bitmapOffset + (byteOffset / size) * size
           buffer = outputFile.getChannel.map(MapMode.READ_WRITE, pos, size)
           buffer.load()
         }
@@ -94,11 +106,12 @@ class BitVectorEncoding extends Encoding {
 
         offset += 1
       })
-
+      outputFile.setLength(fileSize)
       outputFile.close();
 
     } finally {
       source.close();
+      source2.close();
     }
   }
 }
