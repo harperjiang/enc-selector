c7c7e17259bd11292879a0ddaf141322ef8be728
Hao Jiang <harperjiang@msn.com>
2017-09-25 11:29:33 -0500

    Update DB backup script

--- src/main/shell/daily_db_backup.sh
+++ src/main/shell/daily_db_backup.sh
@@ -3,10 +3,13 @@
 # Backup Database
 cd `dirname $0`
 cd ../sql
-mysqldump -u encsel -pencsel encsel > backup_`date +%Y%m%d`.sql
+SQL_DUMP = backup_`date +%Y%m%d`.sql
+mysqldump -u encsel -pencsel encsel > ${SQL_DUMP}
 
+tar cvzf ${SQL_DUMP}.tar.gz ${SQL_DUMP}
+rm ${SQL_DUMP}
 # Delete files older than 7 days
-find . -name 'backup_*.sql' -mtime +7 -delete
+find . -name 'backup_*.sql.tar.gz' -mtime +7 -delete
 
 # Upload to GIT
 git add -A .
