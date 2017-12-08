8d1cdc90bcb081f5266f46fb7af26e9725fa479b
Hao Jiang <harperjiang@msn.com>
2017-12-08 08:54:34 -0600

    Migrate to TF

--- src/main/python/app/train_int_selector.py
+++ src/main/python/app/train_int_selector.py
@@ -1,27 +1,17 @@
 import numpy as np
 import tensorflow as tf
-
+from encsel import graph
 hidden_dim = 800
 
 num_feature = 13
 num_class = 5
 
-input = tf.placeholder(tf.float32, shape=[None, num_feature])
-label = tf.placeholder(tf.float32, shape=[None, num_class])
+train_writer = tf.summary.FileWriter("/home/harper/tftest")
+g = graph.int_graph(hidden_dim)
 
-W1 = tf.Variable([num_feature, hidden_dim])
-b1 = tf.Variable([hidden_dim])
+with tf.Session() as sess:
 
-W2 = tf.Variable([hidden_dim, num_class])
-b2 = tf.Variable([num_class])
-
-layer1 = tf.matmul(input, W1) + b1
-
-layer2 = tf.matmul(tf.tanh(layer1), W2) + b2
-
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=layer2))
-
-train = tf.train.AdamOptimizer(learning_rate=0.01, beta1=0.9, beta2=0.999).minimize(loss)
+    train_writer.add_graph(sess.graph)
 
 # for _ in range(1000):
 
--- src/main/python/encsel/graph.py
+++ src/main/python/encsel/graph.py
@@ -1,24 +1,28 @@
 import tensorflow as tf
 
-def build_graph(num_feature,num_class,hidden_dim):
-    input = tf.placeholder(tf.float32, shape=[None, num_feature])
-    label = tf.placeholder(tf.float32, shape=[None, num_class])
 
-    w1 = tf.Variable([num_feature, hidden_dim])
-    b1 = tf.Variable([hidden_dim])
+def build_graph(num_feature, num_class, hidden_dim):
+    with tf.name_scope("graph"):
+        x = tf.placeholder(tf.float32, shape=[None, num_feature], name="x")
+        y = tf.placeholder(tf.float32, shape=[None, num_class], name="y")
+        with tf.name_scope("layer1"):
+            w1 = tf.Variable(tf.truncated_normal([num_feature,hidden_dim],stddev=0.1), name="w1")
+            b1 = tf.Variable(tf.zeros([hidden_dim]), name="b1")
+            layer1 = tf.tanh(tf.matmul(x, w1) + b1,name="tanh")
+        with tf.name_scope("layer2"):
+            w2 = tf.Variable(tf.truncated_normal([hidden_dim, num_class],stddev=0.1), name="w2")
+            b2 = tf.Variable(tf.zeros([num_class]), name="b2")
 
-    w2 = tf.Variable([hidden_dim, num_class])
-    b2 = tf.Variable([num_class])
+            layer2 = tf.matmul(layer1, w2) + b2
 
-    layer1 = tf.matmul(input, w1) + b1
-    layer2 = tf.matmul(tf.tanh(layer1), w2) + b2
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=layer2),name="loss")
 
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=layer2))
+        return loss
 
-    return loss
 
 def int_graph(hidden_dim):
-    return build_graph(13,5,hidden_dim)
+    return build_graph(13, 5, hidden_dim)
+
 
 def str_graph(hidden_dim):
-    return build_graph(10,4,hidden_dim)
+    return build_graph(10, 4, hidden_dim)
